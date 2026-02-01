4bc9771cd58fd94af34845a49487a739ae2b9154
abhaynaik-dev <abhaynaik9008@gmail.com>
2026-02-01 11:48:20 +0530

    feat: add LruCacheData and public thread-safe LruCache
    Add basic unit tests
    
    Update .gitignore

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,4 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+mod lru_data;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod lru;
+pub use crate::lru::LruCache;
---
+++ src/lru.rs
@@ -0,0 +1,41 @@
+use std::hash::Hash;
+use std::sync::Mutex;
+
+use crate::lru_data::LruCacheData;
+
+pub struct LruCache<K, V> {
+    data: Mutex<LruCacheData<K, V>>,
+}
+
+impl<K: Eq + Hash + Clone, V: Clone> LruCache<K, V> {
+    pub fn new(capacity: usize) -> Self {
+        Self {
+            data: Mutex::new(LruCacheData::new(capacity)),
+        }
+    }
+
+    pub fn get(&self, key: &K) -> Option<V> {
+        let mut guard = self.data.lock().unwrap();
+        guard.get(key).cloned()
+    }
+
+    pub fn put(&self, key: K, value: V) {
+        let mut guard = self.data.lock().unwrap();
+        guard.put(key, value);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::LruCache;
+
+    #[test]
+    fn test_basic_put_get() {
+        let cache = LruCache::new(2);
+        cache.put("k1", "v1");
+        cache.put("k2", "v2");
+
+        assert_eq!(cache.get(&"k1"), Some("v1"));
+        assert_eq!(cache.get(&"k2"), Some("v2"));
+    }
+}
